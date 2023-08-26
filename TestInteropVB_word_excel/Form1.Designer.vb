aa156a56b0e0895480903d3634f44ec669c44bee
SANDRA ITIEL VAZQUEZ QUINTANILLA <vazquez.itiel@uadec.edu.mx>
2023-08-25 23:31:30 -0600

    Agregar archivos de proyecto.

---
+++ TestInteropVB_word_excel/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnWord = New System.Windows.Forms.Button()
+        Me.btnExcel = New System.Windows.Forms.Button()
+        Me.TxtDato = New System.Windows.Forms.TextBox()
+        Me.TxtDatoE = New System.Windows.Forms.TextBox()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'btnWord
+        '
+        Me.btnWord.Location = New System.Drawing.Point(476, 96)
+        Me.btnWord.Name = "btnWord"
+        Me.btnWord.Size = New System.Drawing.Size(99, 33)
+        Me.btnWord.TabIndex = 0
+        Me.btnWord.Text = "Guardar word"
+        Me.btnWord.UseVisualStyleBackColor = True
+        '
+        'btnExcel
+        '
+        Me.btnExcel.Location = New System.Drawing.Point(476, 218)
+        Me.btnExcel.Name = "btnExcel"
+        Me.btnExcel.Size = New System.Drawing.Size(99, 40)
+        Me.btnExcel.TabIndex = 1
+        Me.btnExcel.Text = "Guardar Excel"
+        Me.btnExcel.UseVisualStyleBackColor = True
+        '
+        'TxtDato
+        '
+        Me.TxtDato.Location = New System.Drawing.Point(180, 96)
+        Me.TxtDato.Name = "TxtDato"
+        Me.TxtDato.Size = New System.Drawing.Size(100, 22)
+        Me.TxtDato.TabIndex = 2
+        Me.TxtDato.Text = "Hola Mundo"
+        '
+        'TxtDatoE
+        '
+        Me.TxtDatoE.Location = New System.Drawing.Point(180, 218)
+        Me.TxtDatoE.Name = "TxtDatoE"
+        Me.TxtDatoE.Size = New System.Drawing.Size(100, 22)
+        Me.TxtDatoE.TabIndex = 3
+        Me.TxtDatoE.Text = "Hola Mundo"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TxtDatoE)
+        Me.Controls.Add(Me.TxtDato)
+        Me.Controls.Add(Me.btnExcel)
+        Me.Controls.Add(Me.btnWord)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnWord As Button
+    Friend WithEvents btnExcel As Button
+    Friend WithEvents TxtDato As TextBox
+    Friend WithEvents TxtDatoE As TextBox
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ TestInteropVB_word_excel/Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+    Private Sub btnWord_Click(sender As Object, e As EventArgs) Handles btnWord.Click
+        Dim word = New Microsoft.Office.Interop.Word.Application
+        Dim wordApp = word.Application()
+
+        Dim ruta, dato As String
+
+        If (SaveFileDialog1.ShowDialog() <> DialogResult.OK) Then
+
+        End If
+
+        ruta = SaveFileDialog1.FileName
+        wordApp.Visible = True
+        wordApp.Documents.Add()
+        dato = TxtDato.Text
+
+        wordApp.Selection.TypeText(dato)
+        wordApp.ActiveDocument.SaveAs2(ruta)
+
+    End Sub
+
+    Private Sub btnExcel_Click(sender As Object, e As EventArgs) Handles btnExcel.Click
+        Dim excel = New Microsoft.Office.Interop.Excel.Application
+        Dim excelApp = excel.Application()
+
+        Dim ruta As String
+
+        If (SaveFileDialog1.ShowDialog() <> DialogResult.OK) Then
+
+            Return
+
+        End If
+
+        ruta = SaveFileDialog1.FileName
+        excelApp.Visible = True
+
+        Dim workbook = excelApp.Workbooks.Add()
+        Dim worksheet = workbook.ActiveSheet
+
+
+        worksheet.cells(1, 1) = "Hola Mundo"
+        worksheet.cells(2, 1) = TxtDatoE.Text
+
+
+        workbook.SaveAs(ruta)
+
+    End Sub
+End Class
